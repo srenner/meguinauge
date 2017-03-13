89e5704441afb3cdeb5c8319c040fe9a8cf3f95c
Shawn Renner <shawn.renner@gmail.com>
2017-03-12 21:58:19 -0500

    start playing with can bus

--- main/main.ino
+++ main/main.ino
@@ -3,7 +3,6 @@
 #include <global.h>
 #include <mcp2515.h>
 #include <mcp2515_defs.h>
-
 #include <LiquidCrystal.h>
 #include <math.h>
 
@@ -67,6 +66,10 @@ byte fill5[8] = {
   B11111
 };
 
+
+char can_buffer[64];
+char can_data;
+
 //possible parameters to display
 double  engine_afr;         //AFR
 double  engine_afr_tgt;     //AFR target
@@ -79,7 +82,10 @@ double  engine_vbat;        //battery voltage
 int     engine_clt;         //coolant temp
 int     engine_iat;         //intake air temp
 
+
+
 void setup() {
+
 
   lcd.createChar(0, fill1);
   lcd.createChar(1, fill2);
@@ -97,9 +103,36 @@ void setup() {
   pinMode(BLUELITE, OUTPUT);
   
   setBacklight(255, 0, 0);
+
+  Serial.begin(9600);
+
+  //Initialize CAN Controller 
+  if(Canbus.init(CANSPEED_500))  /* Initialize MCP2515 CAN controller at the specified speed */
+  {
+    Serial.println("CAN Init Ok!");
+    //delay(1500);
+  }
+  else {
+    Serial.println("Can't init CAN");
+  }
+
+  
 }
 
 void loop() {
+
+  ecu_req(can_buffer);
+  //Serial.print("buffer: ");
+  //Serial.println(can_buffer);
+  //Canbus.ecu_req(ENGINE_RPM,buffer);
+  //Canbus.message_rx(buffer);
+  
+  //engine_rpm = buffer;
+  //Serial.print("Engine RPM: "); //Uncomment for Serial debugging
+  //Serial.println("buffer: " + buffer);
+  delay(1500);
+
+  
   double afr = 12.5;
   double tgt = 14.7;
   
@@ -142,3 +175,77 @@ void setBacklight(uint8_t r, uint8_t g, uint8_t b) {
   analogWrite(BLUELITE, b);
 }
 
+char ecu_req(char *buffer) 
+{
+  tCAN message;
+  float engine_data;
+  int timeout = 0;
+  char message_ok = 0;
+  // Prepair message
+  message.id = PID_REQUEST;
+  message.header.rtr = 0;
+  message.header.length = 8;
+  message.data[0] = 0x02;
+  message.data[1] = 0x01;
+  message.data[2] = "all";
+  message.data[3] = 0x00;
+  message.data[4] = 0x00;
+  message.data[5] = 0x00;
+  message.data[6] = 0x00;
+  message.data[7] = 0x00;           
+  
+
+  mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
+  
+  if (mcp2515_send_message(&message)) {
+    Serial.println("message sent");
+  }
+  else {
+    Serial.println("message not sent");
+  }
+        if (mcp2515_check_message()) 
+        {
+          Serial.println("after check message");
+          if (mcp2515_get_message(&message)) 
+          {
+                  engine_data =  ((message.data[3]*256) + message.data[4])/4;
+                  sprintf(buffer,"%d rpm ",(int) engine_data);
+                  Serial.print("buffer 1: ");
+                  Serial.println(buffer);
+              
+                  engine_data =  message.data[3] - 40;
+                  sprintf(buffer,"%d degC",(int) engine_data);
+                  Serial.print("buffer 1: ");
+                  Serial.println(buffer);
+                                
+                  engine_data =  message.data[3];
+                  sprintf(buffer,"%d km ",(int) engine_data);
+                  Serial.print("buffer 1: ");
+                  Serial.println(buffer);
+
+                  engine_data =  ((message.data[3]*256) + message.data[4])/100;
+                  sprintf(buffer,"%d g/s",(int) engine_data);
+                  Serial.print("buffer 1: ");
+                  Serial.println(buffer);
+                  
+                  engine_data = message.data[3]*0.005;
+                  sprintf(buffer,"%d V",(int) engine_data);
+                  Serial.print("buffer 1: ");
+                  Serial.println(buffer);
+                  
+                  engine_data = (message.data[3]*100)/255;
+                  sprintf(buffer,"%d %% ",(int) engine_data);
+                  Serial.print("buffer 1: ");
+                  Serial.println(buffer);
+                  
+            return buffer;
+          }
+          else {
+            Serial.println("nothing to get");
+          }
+        }
+        else {
+          Serial.println("check/else");
+        }
+
+}
